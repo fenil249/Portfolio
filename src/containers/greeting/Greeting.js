1e05ac13a907255fb361be60dc35132a3e1360fc
Fenil <201901130@daiict.ac.in>
2024-02-01 01:18:35 +0530

    Make changes in Greeting.js file

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -2,12 +2,10 @@ import React, {useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
-import landingPerson from "../../assets/lottie/landingPerson";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
 
-import {illustration, greeting} from "../../portfolio";
+
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Greeting() {
